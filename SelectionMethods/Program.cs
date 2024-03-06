b9059358181f829829ea60ebc84ca8a0b0ebda60
SianaStoicheva <ss12712978@edu1.pgzaimov.com>
2024-03-06 17:03:18 +0200

    newwwwwwwwwww

---
+++ SelectionMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SelectionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 2, 4, -5, 1, 10 };
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int k = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[k])
+                        k = j;
+                }
+                int swapVar = arr[i];
+                arr[i] = arr[k];
+                arr[k] = swapVar;
+            }
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+
+    }
+}
